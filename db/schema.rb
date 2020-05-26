593deb62e7ea093c63dd8f08670f6c9fbf612860
Simon Leiva <leivaprogramming@gmail.com>
2020-05-26 17:41:05 -0400

    finished seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_181235) do
+ActiveRecord::Schema.define(version: 2020_05_26_181718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,10 +87,21 @@ ActiveRecord::Schema.define(version: 2020_05_26_181235) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "watched_stocks", force: :cascade do |t|
+    t.bigint "stock_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_watched_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_watched_stocks_on_user_id"
+  end
+
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "hot_stocks", "stocks"
   add_foreign_key "hot_stocks", "users"
   add_foreign_key "positions", "stocks"
   add_foreign_key "posts", "users"
+  add_foreign_key "watched_stocks", "stocks"
+  add_foreign_key "watched_stocks", "users"
 end
