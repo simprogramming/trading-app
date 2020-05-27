270fbfe5780093c94429071e9ed91cc477eb32ff
Simon Leiva <leivaprogramming@gmail.com>
2020-05-27 15:57:14 -0400

    profil page almost done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_164501) do
+ActiveRecord::Schema.define(version: 2020_05_27_182340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 2020_05_27_164501) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "symbol"
+    t.string "category"
     t.index ["stock_id"], name: "index_watched_stocks_on_stock_id"
     t.index ["user_id"], name: "index_watched_stocks_on_user_id"
   end
