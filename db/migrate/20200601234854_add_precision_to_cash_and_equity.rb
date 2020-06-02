e714cc89c4acd11819c3004d6d5e44817999be48
Simon Leiva <leivaprogramming@gmail.com>
2020-06-02 11:37:20 -0400

    finished back end dashboard

--- db/migrate/20200601234854_add_precision_to_cash_and_equity.rb
+++
@@ -1,6 +0,0 @@
-class AddPrecisionToCashAndEquity < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :cash, default: 100000, precision: 2
-    change_column :users, :equity, default: 0, precision: 2
-  end
-end
