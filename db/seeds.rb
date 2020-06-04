6da308e28524de6d4ffeffa3c611e27f6a22bbac
Adrien Laudadio <adrien.laudadio.b@gmail.com>
2020-06-04 16:40:00 -0400

    seeeeeeeeeeeeeeeeeeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,42 +53,45 @@ sector = ['Technology', 'Communication', 'Industrial Goods', 'Utilities', 'Basic
 simon = User.create!(
   email: 'sim@email.com',
   description: 'I am looking to learn more about the Technology sector. I would like to have 5 active positions at all time',
-  nickname: 'Sim',
+  nickname: 'Simer',
   category: 'Technology',
   password: '123456'
   )
 
 claude = User.create!(
   email: 'claude@email.com',
-  description: 'I am looking to learn more about the Technology sector. I would like to have 5 active positions at all time',
-  nickname: 'Sim',
-  category: 'Technology',
+  description: Faker::GreekPhilosophers.quote,
+  nickname: 'Claudius',
+  category: 'Communication',
   password: '123456'
   )
 
 alain = User.create!(
   email: 'alain@email.com',
-  description: 'I am looking to learn more about the Technology sector. I would like to have 5 active positions at all time',
-  nickname: 'Sim',
-  category: 'Technology',
+  description: Faker::GreekPhilosophers.quote,
+  nickname: 'Le Sage',
+  category: 'Financial',
   password: '123456'
   )
 
 normand = User.create!(
   email: 'normand@email.com',
-  description: 'I am looking to learn more about the Technology sector. I would like to have 5 active positions at all time',
-  nickname: 'Sim',
+  description: Faker::GreekPhilosophers.quote,
+  nickname: 'Norm',
   category: 'Technology',
   password: '123456'
   )
 
 denis = User.create!(
   email: 'denis@email.com',
-  description: 'I am looking to learn more about the Technology sector. I would like to have 5 active positions at all time',
-  nickname: 'Sim',
-  category: 'Technology',
+  description: Faker::GreekPhilosophers.quote,
+  nickname: 'Yoda',
+  category: 'Consumer Discretionnary',
   password: '123456'
   )
+
+# --------------------------------------------------------------------
+
 
 symbol = HotStock.create!(
   stock: Stock.find_by_symbol('AMZN'),
@@ -131,10 +134,65 @@ position_3 = Position.create!(
   baseline: 1125,
   target: 1500,
   buy_sell: "Buy",
-  user: simon,
+  user: denis,
   created_at: Date.today - 75.days + 15.5.hours
   )
 prepare_position(position_3)
+
+position_4 = Position.create!(
+  stock: Stock.find_by_symbol('BAC'),
+  entry: 19.5,
+  size: 100,
+  stop_loss: 19.15,
+  baseline: 19.30,
+  target: 25,
+  buy_sell: "Buy",
+  user: alain,
+  created_at: Date.today - 65.days + 14.5.hours
+  )
+prepare_position(position_4)
+
+position_5 = Position.create!(
+  stock: Stock.find_by_symbol('SNAP'),
+  entry: 16,
+  size: 300,
+  stop_loss: 15.90,
+  baseline: 15.95,
+  target: 20,
+  buy_sell: "Buy",
+  user: claude,
+  created_at: Date.today - 15.days + 13.5.hours
+  )
+prepare_position(position_5)
+
+position_6 = Position.create!(
+  stock: Stock.find_by_symbol('HD'),
+  entry: 180,
+  size: 40,
+  stop_loss: 170.5,
+  baseline: 178,
+  target: 220,
+  buy_sell: "Buy",
+  user: denis,
+  created_at: Date.today - 10.days + 13.5.hours
+  )
+prepare_position(position_6)
+
+position_7 = Position.create!(
+  stock: Stock.find_by_symbol('JPM'),
+  entry: 100,
+  size: 120,
+  stop_loss: 105,
+  baseline: 103,
+  target: 70,
+  buy_sell: "Sell",
+  user: alain,
+  created_at: Date.today - 3.days + 14.5.hours
+  )
+prepare_position(position_7)
+
+
+
 
 10.times do
   user = User.new(
@@ -160,7 +218,19 @@ PriceUpdateJob.perform_now
 
 
 
-position_1.update(current_price: 351)
+position_1.update(current_price: 322.32)
+
+position_2.update(current_price: 864.38)
+
+position_3.update(current_price: 1414.30)
+
+position_4.update(current_price: 26.78)
+
+position_5.update(current_price: 19.26)
+
+position_6.update(current_price: 248.95)
+
+position_7.update(current_price: 106.2)
 
 
 
