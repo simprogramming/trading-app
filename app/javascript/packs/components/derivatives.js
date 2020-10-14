ff539b225181cfa445e91fe0a82e0bb4f7eb4b8d
Simon <leivaprogramming@gmailcom>
2020-10-14 14:49:53 -0400

    padding dashboard

--- app/javascript/packs/components/derivatives.js
+++ app/javascript/packs/components/derivatives.js
@@ -9,12 +9,6 @@ import Day4 from './days/day4'
 import Day5 from './days/day5'
 const root = document.getElementById('root');
 
-const days = { derivatives: JSON.parse(root.dataset.derivatives)};
-const Monday = days.derivatives[0].content;
-const Tuesday = days.derivatives[1].content;
-const Wesnesday = days.derivatives[2].content;
-const Thursday = days.derivatives[3].content;
-const Friday = days.derivatives[4].content
 
 
 export default function DerivativesCalendar() {
@@ -23,11 +17,11 @@ export default function DerivativesCalendar() {
 
   return (
     <div className="d-flex">
-      <Day1 day={Monday} days={days} />
-      <Day2 day={Tuesday} days={days} />
-      <Day3 day={Wesnesday} days={days} />
-      <Day4 day={Thursday} days={days} />
-      <Day5 day={Friday} days={days} />
+      <Day1 />
+      <Day2 />
+      <Day3 />
+      <Day4 />
+      <Day5 />
     </div>
   );
 }
