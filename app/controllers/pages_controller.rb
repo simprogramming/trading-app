29b1a2e6ef7cf436f7c6585df82181090d0fbda1
Simon Leiva <simon@effetmonstre.com>
2021-01-05 17:37:43 -0500

    asc table list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,17 +18,17 @@ class PagesController < ApplicationController
   def dashboard
     @hot_stocks = HotStock.all
     @stock = Stock.new
-    @tech_stocks = Stock.where(category: 'Technology')
-    @financial_stocks = Stock.where(category: 'Financial')
-    @real_estate_stocks = Stock.where(category: 'Real Estate')
-    @telecom_stocks = Stock.where(category: 'Telecommunication')
-    @health_care_stocks = Stock.where(category: 'Healthcare')
-    @discretionary_stocks = Stock.where(category: 'Discretionary')
-    @staple_stocks = Stock.where(category: 'Staple')
-    @utilities_stocks = Stock.where(category: 'Utilities')
-    @energy_stocks = Stock.where(category: 'Energy')
-    @basic_material_stocks = Stock.where(category: 'Basic Material')
-    @industrial_stocks = Stock.where(category: 'Industrial')
-    @other_stocks = Stock.where(category: 'Others')
+    @tech_stocks = Stock.where(category: 'Technology').order(grade: :asc, name: :asc)
+    @financial_stocks = Stock.where(category: 'Financial').order(grade: :asc)
+    @real_estate_stocks = Stock.where(category: 'Real Estate').order(grade: :asc)
+    @telecom_stocks = Stock.where(category: 'Telecommunication').order(grade: :asc)
+    @health_care_stocks = Stock.where(category: 'Healthcare').order(grade: :asc)
+    @discretionary_stocks = Stock.where(category: 'Discretionary').order(grade: :asc)
+    @staple_stocks = Stock.where(category: 'Staple').order(grade: :asc)
+    @utilities_stocks = Stock.where(category: 'Utilities').order(grade: :asc)
+    @energy_stocks = Stock.where(category: 'Energy').order(grade: :asc)
+    @basic_material_stocks = Stock.where(category: 'Basic Material').order(grade: :asc)
+    @industrial_stocks = Stock.where(category: 'Industrial').order(grade: :asc)
+    @other_stocks = Stock.where(category: 'Others').order(grade: :asc)
   end
 end
