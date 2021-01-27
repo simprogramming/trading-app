4c7ca48be5b11dde20a75901e837b25abde98668
Simon Leiva <simon@effetmonstre.com>
2021-01-27 17:24:26 -0500

    fixed stuff

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,41 +4,32 @@ class StocksController < ApplicationController
 
   def new
     @stock = Stock.new
-    tech = Stock.where(category: 'Technology')
-    @tech_stocks = HotStock.where(stock_id: tech)
-    finance = Stock.where(category: 'Financial')
-    @financial_stocks = HotStock.where(stock_id: finance)
-    real_estate = Stock.where(category: 'Real Estate')
-    @real_estate_stocks = HotStock.where(stock_id: real_estate)
-    telecom = Stock.where(category: 'Telecommunication')
-    @telecom_stocks = HotStock.where(stock_id: telecom)
-    healthcare = Stock.where(category: 'Healthcare')
-    @health_care_stocks = HotStock.where(stock_id: healthcare)
-    discretionary = Stock.where(category: 'Discretionary')
-    @discretionary_stocks = HotStock.where(stock_id: discretionary)
-    staple = Stock.where(category: 'Staple')
-    @staple_stocks = HotStock.where(stock_id: staple)
-    utilities = Stock.where(category: 'Utilities')
-    @utilities_stocks = HotStock.where(stock_id: utilities)
-    energy = Stock.where(category: 'Energy')
-    @energy_stocks = HotStock.where(stock_id: energy)
-    basic_material = Stock.where(category: 'Basic Material')
-    @basic_material_stocks = HotStock.where(stock_id: basic_material)
-    industrial = Stock.where(category: 'Industrial')
-    @industrial_stocks = HotStock.where(stock_id: industrial)
-    others = Stock.where(category: 'Others')
-    @other_stocks = HotStock.where(stock_id: others)
+    @tech_stocks = Stock.where(category: 'Technology').order(grade: :asc, name: :asc)
+    @financial_stocks = Stock.where(category: 'Financial').order(grade: :asc)
+    @real_estate_stocks = Stock.where(category: 'Real Estate').order(grade: :asc)
+    @telecom_stocks = Stock.where(category: 'Telecommunication').order(grade: :asc)
+    @health_care_stocks = Stock.where(category: 'Healthcare').order(grade: :asc)
+    @discretionary_stocks = Stock.where(category: 'Discretionary').order(grade: :asc)
+    @staple_stocks = Stock.where(category: 'Staple').order(grade: :asc)
+    @utilities_stocks = Stock.where(category: 'Utilities').order(grade: :asc)
+    @energy_stocks = Stock.where(category: 'Energy').order(grade: :asc)
+    @basic_material_stocks = Stock.where(category: 'Basic Material').order(grade: :asc)
+    @industrial_stocks = Stock.where(category: 'Industrial').order(grade: :asc)
+    @other_stocks = Stock.where(category: 'Others').order(grade: :asc)
   end
 
   def create
-    @stock = Stock.new(stock_params)
-    if @stock.save
-      redirect_to new_stock_path
-      flash[:notice] = 'New stock added to database'
-    else
-      redirect_to new_stock_path
-      flash[:notice] = 'it didnt work'
-    end
+    @stock = Stock.create(stock_params)
+    redirect_to new_stock_path
+    flash[:notice] = 'New stock added to database'
+    # @stock = Stock.new(stock_params)
+    # if @stock.save
+    #   redirect_to new_stock_path
+    #   flash[:notice] = 'New stock added to database'
+    # else
+    #   redirect_to new_stock_path
+    #   flash[:notice] = 'it didnt work'
+    # end
   end
 
   def import
